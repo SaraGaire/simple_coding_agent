01579127021e6fc30e11b3b4d49cf8f30fb5dba0
Sara.G <151964803+SaraGaire@users.noreply.github.com>
2025-09-04 15:58:49 +0200

    Create context.js

---
+++ context.js
@@ -0,0 +1,21 @@
+import { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const login = (user) => setCurrentUser(user);
+  const logout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ currentUser, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
